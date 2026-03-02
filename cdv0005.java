c6b05a92a6db5169e8f0131e5a77ea983e11ca60
Navya Sree <navyasree9490@gmail.com>
2026-03-02 15:04:50 +0530

    Calculate and print the sum of two numbers

---
+++ cdv0005.java
@@ -0,0 +1,8 @@
+public class cdv0005 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("Sum = " +sum");
+    }
+}
