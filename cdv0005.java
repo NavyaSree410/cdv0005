b34057b8d94632899c01f3b48e20aa400fea0e33
Navya Sree <navyasree9490@gmail.com>
2026-03-02 15:22:30 +0530

    Fix output statement syntax for sum

--- cdv0005.java
+++ cdv0005.java
@@ -3,6 +3,6 @@ public class cdv0005 {
         int a = 10;
         int b = 20;
         int sum = a + b;
-        System.out.println("Sum = " +sum");
+        System.out.println("Sum = " +sum);
     }
 }
